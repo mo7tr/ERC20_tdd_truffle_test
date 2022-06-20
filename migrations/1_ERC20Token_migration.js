006be811269adb06d9aaeafdec4b6a93322b9095
mo7tr <mo7trading@gmail.com>
2022-06-20 19:23:59 +0200

    migration file creation

---
+++ migrations/1_ERC20Token_migration.js
@@ -0,0 +1,16 @@
+const ERC20Token = artifacts.require("ERC20Token");
+const { BN } = require("@openzeppelin/test-helpers");
+
+module.exports = async function (deployer, network, accounts) {
+  console.log("network =>", network);
+  console.log("accounts =>", accounts);
+
+  const _initialsupply = new BN(1000);
+
+  deployer.deploy(ERC20Token, _initialsupply);
+
+  var instance = await ERC20Token.deployed();
+  var balanceOfOwner = await instance.balanceOf(accounts[0]);
+
+  console.log("balanceOfOwner =>", balanceOfOwner.toString());
+};
